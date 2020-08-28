a770c816336b53830e44fe5e69c81a82c5752664
cheunglong <56506287+cheunglong@users.noreply.github.com>
2020-08-28 08:46:55 +0800

    ssd
    SSD tutorial

---
+++ a-PyTorch-Tutorial-to-Object-Detection-master/create_data_lists.py
@@ -0,0 +1,6 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    create_data_lists(voc07_path='/content/a-PyTorch-Tutorial-to-Object-Detection-master/data/VOCdevkit/VOC2007',
+                      voc12_path='/content/a-PyTorch-Tutorial-to-Object-Detection-master/data/VOCdevkit/VOC2012',
+                      output_folder='/content/a-PyTorch-Tutorial-to-Object-Detection-master/output')
